5ec1748905e1c94870a66e5fcbf5a40a25f1fb4f
Nandakishore N M <nmnandakishore@gmail.com>
2018-10-22 05:45:18 +0530

    Added check cms

---
+++ checkcms.php
@@ -0,0 +1,12 @@
+<?php
+/*if(isset($_GET)){
+	http_response_code (200);
+	// $response = new HttpResponse();
+	// $response->setData('true');
+	// return $response->send();
+	echo "true";
+} else {
+	echo 'true';
+}*/
+echo "true";
+?>
